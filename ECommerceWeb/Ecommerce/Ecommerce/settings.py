a3d140ba51a3c3286796d0396483d33ecd35819e
VenkateshKorapati <venkatesh.k1729k@gmail.com>
2022-02-01 01:10:32 +0530

    create items and add images...

--- ECommerceWeb/Ecommerce/Ecommerce/settings.py
+++ ECommerceWeb/Ecommerce/Ecommerce/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 
@@ -123,7 +123,11 @@ STATICFILES_DIRS=[
 BASE_DIR / 'static',
 ]
 
+MEDIA_URL= '/images/'
+MEDIA_ROOT=os.path.join(BASE_DIR ,'static/images')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
--- ECommerceWeb/Ecommerce/store/views.py
+++ ECommerceWeb/Ecommerce/store/views.py
@@ -1,15 +1,38 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render
+from .models import *
+
 def store(request):
-    context={}
-    return render(request, 'store/store.html',context)
+    search = request.GET.get('search')
+    products = Product.objects.all()
+
+    if search != '' and search is not None:
+        products = products.filter(name__icontains=search)
+
+    context= {'products': products}
+    return render(request, 'store/store.html', context)
+
 
 def cart(request):
-    context={}
-    return render(request, 'store/cart.html',context)
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order={'get_cart_items':0,'get_cart_total':0}
+
+    context= {'items': items, 'order':order}
+    return render(request, 'store/cart.html', context)
 
 def checkout(request):
-    context={}
-    return render(request, 'store/checkout.html',context)
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order={'get_cart_items':0,'get_cart_total':0}
 
+    context= {'items': items, 'order':order}
+    return render(request, 'store/checkout.html', context)
