8df4ef3ac27f97d1a13f7ea847c306e5a66eb552
VenkateshKorapati <venkatesh.k1729k@gmail.com>
2022-02-02 19:39:50 +0530

    add to cart Functionality

--- ECommerceWeb/Ecommerce/store/views.py
+++ ECommerceWeb/Ecommerce/store/views.py
@@ -1,15 +1,21 @@
-
+import json
+from django.http import JsonResponse
 from django.shortcuts import render
 from .models import *
+import datetime
 
 def store(request):
-    search = request.GET.get('search')
-    products = Product.objects.all()
-
-    if search != '' and search is not None:
-        products = products.filter(name__icontains=search)
-
-    context= {'products': products}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+        cartItems=order.get_cart_items
+    else:
+        items = []
+        order={'get_cart_items':0,'get_cart_total':0,'shipping':False}
+        cartItems=order['get_cart_items']
+    products=Product.objects.all()
+    context= {'products': products, 'cartItems':cartItems}    
     return render(request, 'store/store.html', context)
 
 
@@ -18,11 +24,12 @@ def cart(request):
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
         items = order.orderitem_set.all()
+        cartItems=order.get_cart_items
     else:
         items = []
-        order={'get_cart_items':0,'get_cart_total':0}
-
-    context= {'items': items, 'order':order}
+        order={'get_cart_items':0,'get_cart_total':0,'shipping':False}
+        cartItems=order['get_cart_items']
+    context= {'items': items, 'order':order,'cartItems':cartItems}
     return render(request, 'store/cart.html', context)
 
 def checkout(request):
@@ -30,9 +37,66 @@ def checkout(request):
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
         items = order.orderitem_set.all()
+        cartItems=order.get_cart_items
     else:
         items = []
-        order={'get_cart_items':0,'get_cart_total':0}
-
-    context= {'items': items, 'order':order}
+        order={'get_cart_items':0,'get_cart_total':0,'shipping':False}
+        cartItems=order['get_cart_items']
+    context= {'items': items, 'order':order,'cartItems':cartItems}
     return render(request, 'store/checkout.html', context)
+
+def updateItem(request):
+    data=json.loads(request.body)
+    productId=data['productId']
+    action=data['action']
+
+    print('Action:',action)
+    print('productId:',productId)
+
+    customer = request.user.customer
+    product = Product.objects.get(id=productId)
+    order, created = Order.objects.get_or_create(customer=customer, complete=False)
+
+    orderItem,created = OrderItem.objects.get_or_create(order=order , product=product)
+
+    if action == 'add':
+        orderItem.quantity = (orderItem.quantity+1)
+    elif action == 'remove':
+        orderItem.quantity = (orderItem.quantity-1)
+
+    orderItem.save()
+    if orderItem.quantity <=0:
+        orderItem.delete()
+
+    return JsonResponse('Item was added' , safe=False)
+
+#from django.views.decorators.csrf import csrf_protect
+#@csrf_protect
+def processOrder(request):
+    transaction_id=datetime.datetime.now().timestamp()
+    data=json.loads(request.body)
+
+    if request.user.is_authenticated:
+        customer =request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        total=float(data['form']['total'])
+        order.transaction_id=transaction_id
+
+        if total == order.get_cart_total:
+            order.complete = True
+        order.save()
+
+        if order.shipping == True:
+            ShippingAddress.objects.create(
+                customer = customer,
+                order = order,
+                address = data['shipping']['address'],
+                city = data['shipping']['city'],
+                state = data['shipping']['state'],
+                zipcode = data['shipping']['zipcode'],
+            )
+
+    else:
+        print('User not logged in')
+    return JsonResponse('Payment complete!!',safe=False)
+    
